47d06359531e7ef9cc84a0a1cf18806f10b958c9
geniterapeuta12-tech <geniterapeuta12@gmail.com>
2026-08-13 21:12:25 -0300

    Use Integra Smart native UI shell

--- app/src/main/java/com/salvadordetexto/app/MainActivity.java
+++ app/src/main/java/com/salvadordetexto/app/MainActivity.java
@@ -1,34 +1,15 @@
 package com.salvadordetexto.app;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
-    private WebView webView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().setStatusBarColor(Color.rgb(15, 92, 74));
-        getWindow().setNavigationBarColor(Color.WHITE);
-        webView = new WebView(this);
-        webView.setBackgroundColor(Color.rgb(247, 248, 244));
-        webView.setWebViewClient(new WebViewClient());
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setAllowFileAccess(true);
-        setContentView(webView);
-        webView.loadUrl("file:///android_asset/index.html");
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) webView.goBack();
-        else super.onBackPressed();
+    AppUi ui;
+    @Override public void onCreate(Bundle b){
+        super.onCreate(b);
+        ui=new AppUi(this);
+        ui.splash();
+        new Handler().postDelayed(()->ui.home(),1000);
     }
 }
