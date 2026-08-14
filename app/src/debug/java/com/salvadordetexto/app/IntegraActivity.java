c6a98b6635b8b8d051eeebe620e4bafb60dd8427
geniterapeuta12-tech <geniterapeuta12@gmail.com>
2026-08-13 21:03:00 -0300

    Add Integra Smart debug activity

---
+++ app/src/debug/java/com/salvadordetexto/app/IntegraActivity.java
@@ -0,0 +1,34 @@
+package com.salvadordetexto.app;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class IntegraActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setStatusBarColor(Color.rgb(15, 92, 74));
+        getWindow().setNavigationBarColor(Color.WHITE);
+        webView = new WebView(this);
+        webView.setBackgroundColor(Color.rgb(247, 248, 244));
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        setContentView(webView);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
